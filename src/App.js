a8165e75c6b2d4e6b5ce77a12aab4e0480e90f8b
Gihan Lahiru <gihanbimsara2001@gmail.com>
2025-07-23 12:04:17 +0530

    replace

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
       <Header />
       <main style={{ marginTop: "70px" }}></main>
       <h1 className="highlight">Discover Sri Lanka Like Never Before</h1>
-      <p>Plan ponna Ramesh your perfect journey with optimized routes, discover hidden gems, experience local culture, and create unforgettable memories in the Pearl of the Indian Ocean.</p>
+      <p>Plan trip your perfect journey with optimized routes, discover hidden gems, experience local culture, and create unforgettable memories in the Pearl of the Indian Ocean.</p>
     <HeroSection/>
     <FeaturesSection/>
     <PopularDestinations/>
